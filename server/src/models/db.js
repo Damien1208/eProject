3d66583a3904adb37047f9c54c8976176a5f881b
Damien1208 <derail.damien@gmail.com>
2020-03-09 16:44:44 +0100

    set env

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
+require('../../../.env')
 
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Geraniums17',
+    password: `${process.env.SQL_SECRET}`,
     database: 'newdb'
 });
 
